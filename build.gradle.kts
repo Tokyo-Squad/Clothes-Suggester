354741f399d0c98d9c224064819fb6f68c3766bd
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-05-09 18:23:36 +0300

    build/initial (#18)
    * add initial steps.
    
    * add dependencies.

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-client-core:2.3.0")
+    implementation("io.ktor:ktor-client-cio:2.3.0")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.0")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+    testImplementation("com.google.truth:truth:1.4.4")
+    testImplementation("io.mockk:mockk:1.14.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    implementation("io.insert-koin:koin-core:4.0.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(22)
+}
