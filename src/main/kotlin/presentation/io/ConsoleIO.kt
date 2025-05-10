920b869f30fb727682375976207787d34d9fe0ec
Mostafa Mohamed Fahmy <Mostafa0Mohamed2002@gmail.com>
2025-05-10 15:28:08 +0300

    add colored console output

--- src/main/kotlin/presentation/io/ConsoleIO.kt
+++ src/main/kotlin/presentation/io/ConsoleIO.kt
@@ -3,4 +3,5 @@ package presentation.io
 interface ConsoleIO {
     fun read(): String
     fun write(message: String)
+    fun writeError(message: String)
 }
--- src/main/kotlin/presentation/io/ConsoleIOImpl.kt
+++ src/main/kotlin/presentation/io/ConsoleIOImpl.kt
@@ -1,10 +1,16 @@
 package presentation.io
 
 class ConsoleIOImpl : ConsoleIO {
-
+    companion object {
+        private const val ANSI_RESET = "\u001B[0m"
+        private const val ANSI_RED = "\u001B[31m"
+    }
     override fun read(): String = readlnOrNull()?.trim() ?: ""
 
     override fun write(message: String) {
         println(message)
+    }
+    override fun writeError(message: String) {
+        println("$ANSI_RED$message$ANSI_RESET")
     }
 }
